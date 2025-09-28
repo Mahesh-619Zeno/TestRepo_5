9c6d759f6ded4a976b501c265228b0eb85649bba
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-09-28 23:19:13 +0530

    uodated the script.js file

--- Weather-App/script.js
+++ Weather-App/script.js
@@ -5,50 +5,51 @@ let weather = {
         "https://api.openweathermap.org/data/2.5/weather?q=" +
           city +
           "&units=metric&appid=" +
-          apiKey 
+          this.apiKey
       )
         .then((response) => {
-          if (response.status == 404) { 
-            alert("City not found!");
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
           }
-          return response.text(); 
+          return response.json();
         })
-        .then((data) => {
-          this.displayWeather(); 
-        });
+        .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
       const { name } = data;
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-
-      document.querySelector(".city").innerHTML = "Weather in " + name;
+      document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".jpeg"; 
-      document.querySelector(".description").innerHTML = descriptn; 
-      document.querySelector(".temp").innerText = temp + "°"; 
-      document.querySelector(".humidty").innerText = 
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
         "Humidity: " + humidity + "%";
       document.querySelector(".wind").innerText =
-        "Wind: " + speed + "km/hr"; 
-      document.querySelector(".weather").classList.remove("loadng"); /
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?weather" + name + "')"; 
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
     search: function () {
-      this.fetchWeather(document.querySelector(".search-barr").value);
+      this.fetchWeather(document.querySelector(".search-bar").value);
     },
   };
   
-  document.querySelector(".search buttonn").addEventListener("click", function () { 
-    weather.serch(); 
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
   });
   
   document
     .querySelector(".search-bar")
-    .addEventListener("keyUp", function (event) { 
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
         weather.search();
       }
     });
-
-  weather.fetchWeather(); 
+  
+  weather.fetchWeather("Patna");
+  
